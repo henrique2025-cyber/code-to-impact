13d9cd8697b39ad14e47255b49d0146b0e706e6c
henrique2025-cyber <henrique@papx.com.br>
2025-12-10 21:32:32 -0300

    Create grana_robux.py
    my first of many project that'll be so famous and good and they will remember saying: "wow!"

---
+++ grana_robux.py
@@ -0,0 +1,7 @@
+grana = int(input("Qual sua grana robuxera? "))
+if grana > 1000:
+    print("voce é rico,pode entrar")
+    print("voce tem,",grana,"robux")
+else:
+    print("voce é pobre, nao pode entrar")
+    print("voce é tão pobre que não consegui nem pagar imposto do Brasil")
