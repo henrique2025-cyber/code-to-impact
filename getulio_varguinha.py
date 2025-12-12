c9aee9cb82c387c88161c7308bc4de8c66438439
henrique2025-cyber <henrique@papx.com.br>
2025-12-12 08:58:32 -0300

    Create getulio_varguinha.py

---
+++ getulio_varguinha.py
@@ -0,0 +1,13 @@
+print("maquina automatica de zoeira")
+numero = int(input("Escolha um número de 1 a 4: "))
+amigo = input(print("nome do amigo"))
+if numero == 1:
+    print(amigo +" Que isso, virou pato ninja agora?") #a concatenação junta o input com o print
+elif numero == 2:
+    print(amigo +" se voce for na rua te confundem com um gambá") # gambá de tão sujo e podre
+elif numero == 3:
+    print(amigo +" quebrou a matrix de tão feio que é")# o python não ve então posso colocar o que eu quiser
+elif numero == 4:
+    print(amigo+" bla bla bla, agora traduz para o portugues")
+else:
+    print("seu burro esse numero é invalido!")
