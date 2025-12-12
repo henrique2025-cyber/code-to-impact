5006ab48a0b542949574cf036a59556d632c1eb5
henrique2025-cyber <henrique@papx.com.br>
2025-12-12 11:18:31 -0300

    Create youtube_channel.py

---
+++ youtube_channel.py
@@ -0,0 +1,13 @@
+trilha = int(input("how many followers do you have ?"))
+if trilha > 4500:
+  print("listen to the easier way")
+elif trilha > 10000:
+  print("you are in the path to turn more famous, but you need to be better")
+elif trilha > 20000:
+  print("you're doing great, try hard!")
+elif trilha > 1000000:
+  print("how could you do this?")
+elif trilha > 2000000:
+  print("ma'am you are so good")
+else:
+  print("start hard, ok?")
