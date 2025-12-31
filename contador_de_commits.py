5487339b7c6862bed460990a2f16de0d0ac5307b
henrique2025-cyber <henrique@papx.com.br>
2025-12-31 11:00:02 -0300

    Create contador_de_commits.py

---
+++ contador_de_commits.py
@@ -0,0 +1,6 @@
+commit = int(input(print("quantos commits voce fez hoje?")))
+if commit >= 2:
+    print("tem que fazer mais commits para poder jogar chess.com")
+elif commit < 2:
+    print("tem que fazer mais commits no git hub!")
+print("obrigadoo pela a atenção!")
