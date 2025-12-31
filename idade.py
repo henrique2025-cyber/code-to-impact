ca5a7356db4d28f450eabf313036f7f451659e0b
henrique2025-cyber <henrique@papx.com.br>
2025-12-30 21:25:30 -0300

    Create idade.py

---
+++ idade.py
@@ -0,0 +1,6 @@
+idade = int(input(print("qual sua idade?")))
+idade_1 = 18 - idade
+if idade >= 18:
+    print("boa, já está velho o suficiente.")
+else:
+    print("ei voce está novo. faltam ,",idade_1,"anos para 18 anos.")
