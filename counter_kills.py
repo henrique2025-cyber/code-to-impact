0d8615c9b41c84929848c95f68431da8df6e457c
henrique2025-cyber <henrique@papx.com.br>
2026-01-13 16:36:10 -0300

    Create counter_kills.py

---
+++ counter_kills.py
@@ -0,0 +1,10 @@
+kills = int(input(print(("quantas kills hoje no call of duty:"))))
+if kills >10:
+  print("poucos fazem!")
+if kills >5: 
+  print("uau, precisa melhora um pouco!")
+if kills >20:
+  print("absolute cinema!")
+if kills >30:
+  print("impossivel!")
+print("com muitas balas, tchau!")
