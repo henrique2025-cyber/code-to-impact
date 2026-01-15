e408aef355641b347bd0f91702aff657cf0f1acf
henrique2025-cyber <henrique@papx.com.br>
2026-01-15 10:31:44 -0300

    Create horas_do_game.py

---
+++ horas_do_game.py
@@ -0,0 +1,8 @@
+jogo = int(input(print("quantos minutos voce jogou hoje:  ")))
+if jogo <= 60 :
+    print("voce tem que estudar 1 hora e um commit!")
+elif jogo <= 90 :
+    print(" voce tem que estudar 1h30min e 2 commits!")
+elif jogo <= 120 :
+    print(" que isso, 2 horas de estudo e 4 commits!")
+print("fim do programa!") 
